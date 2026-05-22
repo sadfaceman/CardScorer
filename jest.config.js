c296d14821fb329f4c8c0e3e2b839f1ebfc4f396
sadfaceman <noticon@live.dk>
2026-05-22 07:56:15 +0200

    Added jsdom for being able to add more unit test

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,16 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   testMatch: ["**/?(*.)+(spec|test).ts"],
   moduleFileExtensions: ["ts", "js", "json"],
   moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1"
+    "^(\\.{1,2}/.*)\\.js$": "$1",
   },
   transform: {
-    "^.+\\.ts$": ["ts-jest", { tsconfig: "tsconfig.test.json" }]
+    "^.+\\.ts$": ["ts-jest", { tsconfig: "tsconfig.test.json" }],
   },
-  collectCoverageFrom: [
-    "src/**/*.ts",
-    "!src/**/*.d.ts"
-  ],
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"],
   coverageDirectory: "coverage",
-  coverageReporters: ["text", "text-summary", "html", "lcov"]
+  coverageReporters: ["text", "text-summary", "html", "lcov"],
 };
